9f18cbfbc633cc34969231b1586e860bd6ead142
GiverPlay007 <play.giver.007@gmail.com>
2021-01-11 17:05:07 -0300

    Create block registry

--- src/main/java/net/dinastiafoca/world/Block.java
+++ src/main/java/net/dinastiafoca/world/Block.java
@@ -1,5 +1,26 @@
 package net.dinastiafoca.world;
 
-public interface Block {
+import net.dinastiafoca.window.renderer.Renderer;
 
+public abstract class Block {
+
+  public static final int BLOCK_SIZE = 16;
+
+  private final int ID;
+
+  public Block(int id) {
+    this.ID = id;
+  }
+
+  public void update(){
+
+  }
+
+  public void render(Renderer renderer, int x, int y) {
+
+  }
+
+  public int getID() {
+    return ID;
+  }
 }
