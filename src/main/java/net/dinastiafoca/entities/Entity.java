ac23be10bc0ea91e819a8b23971fa0f9e5a06418
GiverPlay007 <play.giver.007@gmail.com>
2021-01-11 17:26:02 -0300

    Entities: Create getters and setters; Add world as property;

--- src/main/java/net/dinastiafoca/entities/Entity.java
+++ src/main/java/net/dinastiafoca/entities/Entity.java
@@ -1,14 +1,138 @@
 package net.dinastiafoca.entities;
 
-public abstract class Entity {
-    public int x;
-    public int y;
+import net.dinastiafoca.window.renderer.Renderer;
+import net.dinastiafoca.world.World;
 
-    public Entity(int x, int y) {
+import java.awt.Rectangle;
+
+public abstract class Entity {
+
+    protected World world;
+
+    protected int width;
+    protected int height;
+
+    protected int x;
+    protected int y;
+
+    protected int maskX;
+    protected int maskY;
+    protected int maskWidth;
+    protected int maskHeight;
+
+    public Entity(World world, int x, int y, int width, int height) {
+        this.world = world;
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
     public void update() {
+
+    }
+
+    public void render(Renderer renderer) {
+
+    }
+
+    public boolean isColliding(Entity entity)
+    {
+        return getHitBox().intersects(entity.getHitBox());
+    }
+
+    public Rectangle getHitBox()
+    {
+        return new Rectangle(x + maskX, y + maskX, width - maskWidth, height - maskHeight);
+    }
+
+    public void setWorld(World world)
+    {
+        this.world = world;
+    }
+
+    public World getWorld()
+    {
+        return world;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getMaskX()
+    {
+        return maskX;
+    }
+
+    public void setMaskX(int maskX)
+    {
+        this.maskX = maskX;
+    }
+
+    public int getMaskY()
+    {
+        return maskY;
+    }
+
+    public void setMaskY(int maskY)
+    {
+        this.maskY = maskY;
+    }
+
+    public int getMaskWidth()
+    {
+        return maskWidth;
+    }
+
+    public void setMaskWidth(int maskWidth)
+    {
+        this.maskWidth = maskWidth;
+    }
+
+    public int getMaskHeight()
+    {
+        return maskHeight;
+    }
+
+    public void setMaskHeight(int maskHeight)
+    {
+        this.maskHeight = maskHeight;
     }
 }
--- src/main/java/net/dinastiafoca/entities/LivingEntity.java
+++ src/main/java/net/dinastiafoca/entities/LivingEntity.java
@@ -1,16 +1,39 @@
 package net.dinastiafoca.entities;
 
+import net.dinastiafoca.world.World;
+
 public abstract class LivingEntity extends Entity {
+
     protected int life;
     protected int maxLife;
 
-    public LivingEntity(int x, int y, int life, int maxLife) {
-        super(x, y);
+    public LivingEntity(World world, int x, int y, int width, int height, int life, int maxLife) {
+        super(world, x, y, width, height);
         this.life = life;
         this.maxLife = maxLife;
     }
 
-    public LivingEntity(int x, int y, int maxLife) {
-        this(x, y, maxLife, maxLife);
+    public LivingEntity(World world, int x, int y, int width, int height, int maxLife) {
+        this(world, x, y, width, height, maxLife, maxLife);
+    }
+
+    public int getLife()
+    {
+        return life;
+    }
+
+    public void setLife(int life)
+    {
+        this.life = life;
+    }
+
+    public int getMaxLife()
+    {
+        return maxLife;
+    }
+
+    public void setMaxLife(int maxLife)
+    {
+        this.maxLife = maxLife;
     }
 }
