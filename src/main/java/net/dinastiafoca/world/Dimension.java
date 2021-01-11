1112072b39db67806c313b129f4fb9a5e0fe0e94
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-11 12:53:20 -0300

    remove comments

--- src/main/java/net/dinastiafoca/world/Dimension.java
+++ src/main/java/net/dinastiafoca/world/Dimension.java
@@ -2,11 +2,7 @@ package net.dinastiafoca.world;
 
 public interface Dimension {
 
-    // A parada da dimension é pra ficar igual no Minecraft mesmo
-    // Que cada mundo tem três dimensões (em situações normais)
-    // Overworld, Nether, The End
-    // Fica bom, né? Tomara que sim...
-    // Já volto
+    World getWorld();
 
     Block getBlock(int x, int y);
 
