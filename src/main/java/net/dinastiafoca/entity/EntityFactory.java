c974f39b115688089a5807626eeaa16b80f29cb6
GiverPlay007 <play.giver.007@gmail.com>
2021-01-15 15:25:43 -0300

    Create EntityFactory

---
+++ src/main/java/net/dinastiafoca/entity/EntityFactory.java
@@ -0,0 +1,24 @@
+package net.dinastiafoca.entity;
+
+import net.dinastiafoca.entity.entities.EntityPlayer;
+import net.dinastiafoca.game.BaseGame;
+import net.dinastiafoca.window.renderer.Sprite;
+import net.dinastiafoca.world.World;
+
+public final class EntityFactory
+{
+  private final World world;
+  private final BaseGame game;
+
+  public EntityFactory(World world, BaseGame game)
+  {
+    this.world = world;
+    this.game = game;
+  }
+
+  public EntityPlayer createPlayer(int x, int y)
+  {
+    Sprite sprite = game.getSpriteSheet().getSprite("entity/player.png");
+    return new EntityPlayer(world, x, y, sprite);
+  }
+}
