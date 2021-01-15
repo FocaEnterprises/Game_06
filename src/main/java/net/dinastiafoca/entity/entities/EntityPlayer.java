4e80ba5b053f6f58ca679ae7a575dbf3f5f773b2
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-14 22:01:36 -0300

    Update EntityPlayer.java

--- src/main/java/net/dinastiafoca/entity/entities/EntityPlayer.java
+++ src/main/java/net/dinastiafoca/entity/entities/EntityPlayer.java
@@ -2,6 +2,7 @@ package net.dinastiafoca.entity.entities;
 
 import net.dinastiafoca.entity.MobEntity;
 import net.dinastiafoca.window.renderer.Renderer;
+import net.dinastiafoca.window.renderer.Sprite;
 import net.dinastiafoca.world.Block;
 import net.dinastiafoca.world.World;
 
@@ -9,9 +10,9 @@ import java.awt.Color;
 
 public class EntityPlayer extends MobEntity
 {
-  public EntityPlayer(World world, int x, int y)
+  public EntityPlayer(World world, int x, int y, Sprite sprite)
   {
-    super(world, x, y, Block.BLOCK_SIZE, Block.BLOCK_SIZE, 10);
+    super(world, x, y, Block.BLOCK_SIZE, Block.BLOCK_SIZE, sprite, 10);
   }
 
   @Override
@@ -23,7 +24,6 @@ public class EntityPlayer extends MobEntity
   @Override
   public void render(Renderer renderer)
   {
-    renderer.setColor(Color.RED);
-    renderer.fillRect(getX(), getY(), getWidth(), getHeight());
+    renderer.drawImage(sprite.getSprite(), x, y, width, height, null);
   }
 }
