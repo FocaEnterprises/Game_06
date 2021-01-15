83956c7365d873a6477fe40babcfd79ca46841b3
GiverPlay007 <play.giver.007@gmail.com>
2021-01-14 22:17:04 -0300

    Refactor: Add Sprite in Block constructor.

--- src/main/java/net/dinastiafoca/world/Block.java
+++ src/main/java/net/dinastiafoca/world/Block.java
@@ -1,19 +1,27 @@
 package net.dinastiafoca.world;
 
 import net.dinastiafoca.window.renderer.Renderer;
+import net.dinastiafoca.window.renderer.Sprite;
 
 public abstract class Block {
 
   public static final int BLOCK_SIZE = 16;
 
+  private final Sprite sprite;
   private final int ID;
 
-  public Block(int id)
-  {
+  public Block(int id, Sprite sprite) {
     this.ID = id;
+    this.sprite = sprite;
   }
 
-  public abstract void render(Renderer renderer, int x, int y);
+  public void render(Renderer renderer, int x, int y) {
+    if(sprite == null){
+      return;
+    }
+
+    renderer.drawImage(sprite.getSprite(), x, y, BLOCK_SIZE, BLOCK_SIZE, null);
+  }
 
   public void update() {
 
--- src/main/java/net/dinastiafoca/world/blocks/AirBlock.java
+++ src/main/java/net/dinastiafoca/world/blocks/AirBlock.java
@@ -1,20 +1,10 @@
 package net.dinastiafoca.world.blocks;
 
-import net.dinastiafoca.window.renderer.Renderer;
 import net.dinastiafoca.world.Block;
-
-import java.awt.Color;
 
 public class AirBlock extends Block {
 
   public AirBlock() {
-    super(0);
-  }
-
-  @Override
-  public void render(Renderer renderer, int x, int y)
-  {
-    renderer.setColor(Color.CYAN);
-    renderer.drawRect(x, y, Block.BLOCK_SIZE, Block.BLOCK_SIZE);
+    super(0, null);
   }
 }
