3255bd3448f57abe4218bfbe98f344f1f9ee8010
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-16 19:16:32 -0300

    Animation Looping

--- src/main/java/net/dinastiafoca/window/renderer/Animation.java
+++ src/main/java/net/dinastiafoca/window/renderer/Animation.java
@@ -1,28 +1,49 @@
 package net.dinastiafoca.window.renderer;
 
+import net.dinastiafoca.threads.Looping;
 import net.dinastiafoca.window.renderer.Sprite;
 
 import java.awt.image.BufferedImage;
 
-public class Animation extends Sprite {
+public class Animation extends Sprite implements Looping {
     public BufferedImage[] sprites;
-    public int index;
-    public int frames;
+    private int index;
+    private int frames;
+    public float speed = 1;
+    private boolean isRunning;
 
-    public Animation(BufferedImage[] sprites) {
+    public Animation(BufferedImage[] sprites, float speed) {
         super(sprites[0]);
         this.sprites = sprites;
+        this.speed = speed;
     }
 
     public BufferedImage getSprite() {
-        frames++;
-        if(frames >= 30){
-            frames = 0;
-            index++;
-            if(index >= sprites.length) {
-                index = 0;
+        if(isRunning) {
+            frames++;
+            if (frames >= 30 / (speed == 0 ? 1 : speed)) {
+                frames = 0;
+                index++;
+                if (index >= sprites.length) {
+                    index = 0;
+                }
             }
         }
         return sprites[index];
+    }
+
+    @Override
+    public void start() {
+        isRunning = true;
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return isRunning;
     }
 }
