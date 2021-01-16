3dc2443c2a1056e9d36ee6075686905aef633edc
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-16 20:12:46 -0300

    Add animation from JSON

---
+++ src/main/java/net/dinastiafoca/window/renderer/AnimationLoader.java
@@ -0,0 +1,58 @@
+package net.dinastiafoca.window.renderer;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class AnimationLoader {
+    public Animation getAnimation(String path) {
+        JSONObject AnimationJSON = new JSONObject(readFile(path));
+        JSONArray spritesJson = AnimationJSON.getJSONArray("frames");
+
+        BufferedImage[] sprites = getSpritesFromJSON(spritesJson);
+        float speed = AnimationJSON.getFloat("speed");
+
+        return new Animation(sprites, speed);
+    }
+
+    private BufferedImage[] getSpritesFromJSON(JSONArray jsonArray) {
+        BufferedImage[] sprites = new BufferedImage[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject frame = jsonArray.getJSONObject(i);
+            String spritePath = frame.getString("path");
+            int x = frame.getInt("x");
+            int y = frame.getInt("y");
+            int width = frame.getInt("width");
+            int height = frame.getInt("height");
+
+            sprites[i] = loadImage(spritePath, x, y, width, height);
+        }
+        return sprites;
+    }
+
+    private String readFile(String path) {
+        InputStream myObj = this.getClass().getResourceAsStream(path);
+        Scanner myReader = new Scanner(myObj);
+        String text = "";
+        while (myReader.hasNextLine()) {
+            text += myReader.nextLine();
+        }
+        myReader.close();
+
+        return text;
+    }
+
+    private BufferedImage loadImage(String path, int x, int y, int width, int height) {
+        try {
+            return ImageIO.read(getClass().getResource(path)).getSubimage(x, y, width, height);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
