2e0565645ac4452833138a25232822fb9cb32c4c
GiverPlay007 <play.giver.007@gmail.com>
2021-01-21 20:33:45 -0300

    Create World and Window getters + edit initial Player position + add seed to World constructor parameters.

--- src/main/java/net/dinastiafoca/game/Game.java
+++ src/main/java/net/dinastiafoca/game/Game.java
@@ -7,7 +7,6 @@ import net.dinastiafoca.input.Keyboard;
 import net.dinastiafoca.window.Camera;
 import net.dinastiafoca.window.Window;
 import net.dinastiafoca.window.renderer.Renderer;
-import net.dinastiafoca.window.renderer.Spritesheet;
 import net.dinastiafoca.world.World;
 import net.dinastiafoca.world.impl.SimpleWorld;
 
@@ -15,6 +14,7 @@ public class Game implements BaseGame {
     public Renderer renderer;
     public Camera camera;
 
+    private Window window;
     private EntityFactory entityFactory;
     private EntityPlayer player;
     private PlayerController controller;
@@ -23,7 +23,7 @@ public class Game implements BaseGame {
 
     @Override
     public void onCreate() {
-        Window window = new Window.WindowBuilder()
+        window = new Window.WindowBuilder()
                 .setTitle("--")
                 .setWidth(800)
                 .setHeight(600)
@@ -31,7 +31,7 @@ public class Game implements BaseGame {
 
         renderer = new Renderer(window);
 
-        world = new SimpleWorld(100, 100);
+        world = new SimpleWorld(250, 250, 256);
 
         camera = new Camera();
 
@@ -40,7 +40,7 @@ public class Game implements BaseGame {
         keyboard = new Keyboard();
         window.addKeyboard(keyboard);
 
-        player = entityFactory.createPlayer(32, 32);
+        player = entityFactory.createPlayer(86, 86);
         controller = new PlayerController(player, this);
 
         world.getDimension(0).addEntity(player);
@@ -67,5 +67,15 @@ public class Game implements BaseGame {
     @Override
     public Keyboard getKeyboard() {
         return keyboard;
+    }
+
+    public World getWorld()
+    {
+        return world;
+    }
+
+    public Window getWindow()
+    {
+        return window;
     }
 }
