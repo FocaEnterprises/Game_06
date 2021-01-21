a2fcc2c3853a0e005e84a9e4908a303e07eea537
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-20 22:27:58 -0300

    OpenSimplexNoise

--- src/main/java/net/dinastiafoca/world/gen/Test.java
+++ src/main/java/net/dinastiafoca/world/gen/Test.java
@@ -8,20 +8,22 @@ import java.util.Random;
 
 public class Test
 {
+  private static final double FEATURE_SIZE = 24;
+
   public static void main(String[] args) throws Exception
   {
     Random rand = new Random();
-    ImprovedNoise noise = new ImprovedNoise(rand.nextDouble());
+    OpenSimplexNoise noise = new OpenSimplexNoise(rand.nextLong());
     Window win = new Window.WindowBuilder().setTitle("Noise").setWidth(720).setHeight(460).build();
     Renderer renderer = new Renderer(win);
 
     while(true) {
-      noise.setSeed(rand.nextDouble());
+      noise = new OpenSimplexNoise(rand.nextLong());
       renderer.begin();
       renderer.setColor(Color.RED);
 
       for(int i = 0; i < win.getWidth(); i++) {
-        int a = (int) (noise.noise(i, 200) * 10);
+        int a = (int) (noise.eval(i / FEATURE_SIZE, 200 / FEATURE_SIZE, 0) * 10);
         renderer.drawLine(i, 200 + a, i, 200 + a);
       }
 
