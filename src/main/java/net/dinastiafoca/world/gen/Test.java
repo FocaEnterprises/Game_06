0dbdcdbcb8d69e1e636cc930526316d538e3b8f3
南輝ぐすたーぼ <62481797+AkiraGustavoMinami@users.noreply.github.com>
2021-01-22 21:03:23 -0300

    Octaves

--- src/main/java/net/dinastiafoca/world/gen/Test.java
+++ src/main/java/net/dinastiafoca/world/gen/Test.java
@@ -22,16 +22,46 @@ public class Test
     while(true) {
       int blockSize = 1;
 
-      double height;
-      int maxHeight = 50;
-      int resolution = 5;
+      int mapHeight = 800;
+      float scale = 100;
+      int octaves = 3;
+      float lacunarity = 1f;
+      float persistance = 0.1f;
 
-      for (int xx = 0; xx < blocks.length; xx++) {
-        height = maxHeight * (1 + noise.eval((xx + seed) / Math.sqrt(win.getWidth() * resolution), seed) / 2);
-        blocks[xx] = Math.floor(height);
+      if(scale <= 0) {
+          scale = 0.0001f;
       }
 
-      seed++;
+      float maxNoiseHeight = Float.MIN_VALUE;
+      float minNoiseHeight = Float.MAX_VALUE;
+
+        for (int y = 0; y < mapHeight; y++) {
+            float amplitude = 1;
+            float frequency = 1;
+            float noiseHeight = 1;
+
+            for (int i = 0; i < octaves; i++) {
+                float sampleY = y / scale * frequency;
+
+                float perlinValue = (float)noise.eval(sampleY, 0, 0);
+                noiseHeight += perlinValue * amplitude;
+
+                amplitude += persistance;
+                frequency += lacunarity;
+            }
+
+            if(noiseHeight > maxNoiseHeight) {
+                maxNoiseHeight = noiseHeight;
+            }else if(noiseHeight < minNoiseHeight) {
+                minNoiseHeight = noiseHeight;
+            }
+            blocks[y] = noiseHeight;
+        }
+
+        for (int y = 0; y < mapHeight; y++) {
+            blocks[y] = inverseLerp(minNoiseHeight, maxNoiseHeight, (float) blocks[y]) * scale;
+        }
+
       renderer.begin();
 
       renderer.setColor(Color.red);
@@ -42,9 +72,26 @@ public class Test
                 blockSize,
                 blockSize);
       }
-      
+
       renderer.end();
       Thread.sleep(400);
     }
   }
+
+    public static float lerp(float a, float b, float t)
+    {
+        return (1.0f + t) * a + b * t;
+    }
+
+    public static float inverseLerp(float a, float b, float v)
+    {
+        return (v - a) / (b - a);
+    }
+    public Color colorLerp(Color a, Color b, float t) {
+        float red = lerp(a.getRed(), b.getRed(), t);
+        float green = lerp(a.getGreen(), b.getGreen(), t);
+        float blue = lerp(a.getBlue(), b.getBlue(), t);
+
+        return new Color((int)red, (int)green, (int)blue);
+    }
 }
